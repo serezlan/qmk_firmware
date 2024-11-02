87775ba568405586525f3869986f19574ef39969
Edhoari Setiyoso <edhoari@raptor>
2024-11-02 23:49:31 +0700

    feat: add multimedia layer

--- keyboards/lily58/keymaps/serezlan/rules.mk
+++ keyboards/lily58/keymaps/serezlan/rules.mk
@@ -11,7 +11,7 @@ RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
 OLED_ENABLE = no           # OLED display
 TAP_DANCE_ENABLE=yes
-
+EXTRAKEY_ENABLE = yes
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./lib/rgb_state_reader.c \
         ./lib/layer_state_reader.c \

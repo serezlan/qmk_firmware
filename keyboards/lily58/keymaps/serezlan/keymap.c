1bce76f18f24c60b4cecc806f956b8b42bf9a0d8
Edhoari Setiyoso <edhoari@raptor>
2024-11-10 06:46:08 +0700

    add symbol layer
    - add escape key on raise layer
    - add application button in base layer

--- keyboards/lily58/keymaps/serezlan/keymap.c
+++ keyboards/lily58/keymaps/serezlan/keymap.c
@@ -2,7 +2,7 @@
 
 enum layer_number {
   _BASE = 0,
-  _NAV,
+  _RAISE,
   _SYMBOL,
   _NUMBER,
   _MEDIA,
@@ -20,12 +20,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,
   KC_BSPC,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  TD(TD_LSFT_INS),  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, MO(_NUMBER),  KC_DEL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-  KC_LALT, KC_LGUI, KC_LCTL, MO(_NAV), KC_ENT, KC_SPC, MO(_MEDIA), KC_RGUI
+  TD(TD_LSFT_INS),  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, MO(_SYMBOL),  KC_DEL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
+  KC_LALT, KC_LGUI, KC_LCTL, MO(_RAISE), KC_ENT, KC_SPC, MO(_MEDIA), KC_APPLICATION
 ),
 
-[_NAV] = LAYOUT(
-  XXXXXXX,   XXXXXXX,   XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,                     KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_F11,    KC_F12,
+[_RAISE] = LAYOUT(
+  KC_ESC,   XXXXXXX,   XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,                     KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_F11,    KC_F12,
   XXXXXXX,   XXXXXXX,   XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,                     KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,
   XXXXXXX,  XXXXXXX,   XXXXXXX,    KC_DEL,    KC_LCTL,    XXXXXXX,                     KC_LEFT,    KC_UP,    KC_DOWN,    KC_RIGHT,    XXXXXXX, XXXXXXX,
   XXXXXXX,  XXXXXXX,   XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,    KC_HOME,    KC_PGUP, KC_PGDN,  KC_END,  QK_LOCK,
@@ -40,12 +40,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   XXXXXXX, XXXXXXX, XXXXXXX,  KC_LSFT, XXXXXXX,  KC_DOT, XXXXXXX, XXXXXXX
 ),
 
+[_SYMBOL] = LAYOUT(
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX,  XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,                        XXXXXXX,    KC_LBRC,    KC_RBRC,    XXXXXXX,    XXXXXXX,    XXXXXXX,
+  XXXXXXX,  XXXXXXX,    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                       KC_BSLS, KC_LPRN, KC_RPRN, XXXXXXX,   KC_RBRC, XXXXXXX,
+  XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, KC_RBRC,
+  XXXXXXX, XXXXXXX, XXXXXXX,  KC_LSFT, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX
+),
+
 [_MEDIA] = LAYOUT(
   KC_F1,   KC_F2,   KC_F3,    KC_F4,    KC_F5,    KC_F6,                     KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_F11,    KC_F12,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
   XXXXXXX,  XXXXXXX,   XXXXXXX,    XXXXXXX,    XXXXXXX,    _______,                     KC_MPRV,    KC_MPLY,    KC_MNXT,    XXXXXXX,    XXXXXXX, XXXXXXX,
   XXXXXXX,  XXXXXXX,   XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX, _______,  _______,  KC_VOLD,    KC_MUTE,    KC_VOLU, XXXXXXX,  XXXXXXX,  XXXXXXX,
-  KC_LALT, KC_LGUI, KC_LCTL, MO(_NAV), KC_ENT, KC_SPC, KC_BSPC, KC_RGUI
+  KC_LALT, KC_LGUI, KC_LCTL, MO(_RAISE), KC_ENT, KC_SPC, KC_RALT, KC_RGUI
 ),
 };
 

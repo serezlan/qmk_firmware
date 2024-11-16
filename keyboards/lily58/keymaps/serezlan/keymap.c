8f9069b12ff4086fde7c8bb24d2fffc82f9ed41a
Edhoari Setiyoso <edhoari@raptor>
2024-11-16 10:29:02 +0700

    fix: remove toggle media layer
    - quote key when single tapped and MO media layer when hold

--- keyboards/lily58/keymaps/serezlan/keymap.c
+++ keyboards/lily58/keymaps/serezlan/keymap.c
@@ -115,17 +115,7 @@ void ql_finished(tap_dance_state_t *state, void *user_data) {
         case TD_SINGLE_HOLD:
             layer_on(_MEDIA);
             break;
-        case TD_DOUBLE_TAP:
-            // Check to see if the layer is already set
-            if (layer_state_is(_MEDIA)) {
-                // If already set, then switch it off
-                layer_off(_MEDIA);
-            } else {
-                // If not already set, then switch the layer on
-                layer_on(_MEDIA);
-            }
-            break;
-        default:
+    default:
             break;
     }
 }

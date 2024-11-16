f0bfeb78f5561b6e5fdd72c4130ac6b8a4e41676
Edhoari Setiyoso <edhoari@raptor>
2024-11-16 12:43:05 +0700

    add mod tap on left ctrl
    - Left ctrl on hold, space on tap

--- keyboards/lily58/keymaps/serezlan/keymap.c
+++ keyboards/lily58/keymaps/serezlan/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,
   KC_BSPC,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, TD(QUOT_MEDIA_LAYR),
   TD(TD_LSFT_INS),  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, TG(_FN),  KC_DEL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-  KC_LALT, KC_LGUI, KC_LCTL, MO(_RAISE), KC_ENT, KC_SPC, KC_RALT, KC_APPLICATION
+  KC_LALT, KC_LGUI, MT(MOD_LCTL, KC_SPC), MO(_RAISE), KC_RALT, KC_SPC, KC_ENT, KC_APPLICATION
 ),
 
 [_RAISE] = LAYOUT(

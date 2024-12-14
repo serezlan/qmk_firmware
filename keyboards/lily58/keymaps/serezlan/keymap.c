a376ced8b53bacb3a2fccd4c3832904e60012535
Edhoari Setiyoso <edhoari@sunshine>
2024-12-14 18:50:15 +0700

    feat: add combo keys

--- keyboards/lily58/keymaps/serezlan/keymap.c
+++ keyboards/lily58/keymaps/serezlan/keymap.c
@@ -64,21 +64,27 @@ void delta_finished(tap_dance_state_t *state, void *user_data);
 void delta_reset(tap_dance_state_t *state, void *user_data);
 
 // combo keys
-const uint16_t PROGMEM test_combo1[] = {KC_D, KC_F, COMBO_END};
-const uint16_t PROGMEM test_combo2[] = {KC_C, KC_D, COMBO_END};
+const uint16_t PROGMEM combo_del [] = {KC_D, KC_F, COMBO_END};
+const uint16_t PROGMEM combo_quote[] = {KC_L, KC_SEMICOLON, COMBO_END};
+const uint16_t PROGMEM combo_lbrc[] = {KC_U, KC_I, COMBO_END};
+const uint16_t PROGMEM combo_rbrc[] = {KC_I, KC_O, COMBO_END};
+const uint16_t PROGMEM combo_bsls[] = {KC_O, KC_P, COMBO_END};
 
 combo_t key_combos[] = {
-    COMBO(test_combo1, KC_DEL),
-    COMBO(test_combo2, LCTL(KC_Z)), // keycodes with modifiers are possible too!
+    COMBO(combo_del, KC_DEL),
+    COMBO(combo_quote, KC_QUOTE), 
+    COMBO(combo_lbrc, KC_LBRC), 
+    COMBO(combo_rbrc, KC_RBRC), 
+    COMBO(combo_bsls, KC_BSLS), 
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
 		   TD(TD_FN1_LAYER),   TD(TD_FN2_LAYER),   TD(TD_FN3_LAYER),    TD(TD_FN4_LAYER),    TD(TD_FN5_LAYER),    TD(TD_FN6_LAYER),                     TD(TD_FN7_LAYER),    TD(TD_FN8_LAYER),    TD(TD_FN9_LAYER),    TD(TD_FN10_LAYER),    TD(TD_FN11_LAYER),    TD(TD_FN12_LAYER),
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,
-  KC_BSPC,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, TD(QUOT_MEDIA_LAYR),
+  KC_BSPC,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
   TD(TD_CHARLIE_LAYER),  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  TD(TD_DELTA_LAYER),
-  KC_LALT, KC_LGUI, KC_LCTL, TD(BETA_LAYER), KC_ENT, KC_SPC, TD(ALPHA_LAYER), KC_APPLICATION
+  KC_LALT, KC_LGUI, KC_LCTL, KC_SPACE, KC_SPC, KC_RALT, TD(ALPHA_LAYER), KC_APPLICATION
 ),
 
 [_RAISE] = LAYOUT(
@@ -175,6 +181,7 @@ static td_tap_t delta_tap_state = {
     .state = TD_NONE
 };
 
+;
 // Functions that control what our tap dance key does
 void ql_finished(tap_dance_state_t *state, void *user_data) {
     ql_tap_state.state = cur_dance(state);

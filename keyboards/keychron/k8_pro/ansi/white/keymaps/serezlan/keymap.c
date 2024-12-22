1478b090df41b03730dfdbf56ec6f0b71a78d6bf
Edhoari Setiyoso <edhoari@sunshine>
2024-12-22 08:51:20 +0700

    feat: both shift has double tap function
    - left shift will act as insert when double tapped
    - right shift will act as shift lock when double tapped

--- keyboards/keychron/k8_pro/ansi/white/keymaps/serezlan/keymap.c
+++ keyboards/keychron/k8_pro/ansi/white/keymaps/serezlan/keymap.c
@@ -14,6 +14,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <stdbool.h>
+#include "keycodes.h"
 #include QMK_KEYBOARD_H
 
 // clang-format off
@@ -25,19 +27,41 @@ enum layers{
   WIN_FN_2
 };
 
+// Define a type for as many tap dance states as you need
+typedef enum {
+    TD_NONE,
+    TD_UNKNOWN,
+    TD_SINGLE_TAP,
+    TD_SINGLE_HOLD,
+    TD_DOUBLE_SINGLE_TAP,
+    TD_DOUBLE_TAP,
+    TD_DOUBLE_HOLD,
+} td_state_t;
+
+typedef struct {
+    bool is_press_action;
+    td_state_t state;
+} td_tap_t;
+
 //Tap Dance Declarations
 enum {
   TD_LSFT_INS = 0,
-  /* TD_LCTL_WIN = 1, */
-  /* TD_LCTL_WIN_FN_2 = 2, */
+  TD_DELTA_LAYER,
 };
 
-//Tap Dance Definitions
-tap_dance_action_t tap_dance_actions[] = {
-  [TD_LSFT_INS]  = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_INS),
-  /* [TD_LCTL_WIN]  = ACTION_TAP_DANCE_LAYER_MOVE(KC_LCTL, WIN_BASE), */
-  /* [TD_LCTL_WIN_FN_2]  = ACTION_TAP_DANCE_LAYER_MOVE(KC_LCTL, WIN_FN_2) */
-/* Other declarations would go here, separated by commas, if you have them */
+// function used by all TD
+td_state_t cur_dance(tap_dance_state_t *state);
+
+// functions associated with each tap dance
+void delta_finished(tap_dance_state_t *state, void *user_data);
+void delta_reset(tap_dance_state_t *state, void *user_data);
+
+// static vars for TD
+static bool isShiftLocked = false;
+
+static td_tap_t delta_tap_state = {
+    .is_press_action = true,
+    .state = TD_NONE
 };
 
 // macro stuff
@@ -88,11 +112,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [WIN_BASE] = LAYOUT_tkl_ansi(
      KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,             KC_PSCR,   KC_CTANA, BL_STEP,
-     KC_INS,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_INS,    KC_HOME,  KC_PGUP,
+     KC_GRAVE,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_INS,    KC_HOME,  KC_PGUP,
      KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_DEL,    KC_SPC,   KC_PGDN,
-     LT(WIN_FN_2, KC_BSPC),  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_SPC,
-     KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,              KC_SPC,
-     KC_LALT,  KC_LGUI,  KC_LCTL,                                KC_SPC,                                 KC_RALT,  KC_APPLICATION, MO(WIN_FN),KC_RCTL,  KC_SPC,  KC_SPC,  KC_SPC),
+     KC_BSPC,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,
+     TD(TD_LSFT_INS),            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            TD(TD_DELTA_LAYER),              KC_UP,
+     KC_LALT,  KC_LGUI,  KC_LCTL,                                KC_SPC,                                 KC_RALT,  KC_APPLICATION, MO(WIN_FN),KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RIGHT
+			     ),
 
 [WIN_FN] = LAYOUT_tkl_ansi(
      KC_ESC,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  BL_DOWN,  BL_UP,    KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,            KC_TRNS,  KC_TRNS,  BL_TOGG,
@@ -118,5 +143,59 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_RGHT,  APPEND_SEMI,  KC_TRNS,            KC_TRNS, // forth row
      KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  APPEND_COMMA,  KC_TRNS,  KC_TRNS,            KC_TRNS,            KC_TRNS,
      KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_ENT,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
-
 };
+
+//Tap Dance Definitions
+tap_dance_action_t tap_dance_actions[] = {
+  [TD_LSFT_INS]  = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_INS),
+      [TD_DELTA_LAYER] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, delta_finished, delta_reset),
+  /* [TD_LCTL_WIN]  = ACTION_TAP_DANCE_LAYER_MOVE(KC_LCTL, WIN_BASE), */
+  /* [TD_LCTL_WIN_FN_2]  = ACTION_TAP_DANCE_LAYER_MOVE(KC_LCTL, WIN_FN_2) */
+/* Other declarations would go here, separated by commas, if you have them */
+};
+
+td_state_t cur_dance(tap_dance_state_t *state) {
+    if (state->count == 1) {
+        if (!state->pressed) return TD_SINGLE_TAP;
+        else return TD_SINGLE_HOLD;
+	} else if (state->count == 2) {
+        // TD_DOUBLE_SINGLE_TAP is to distinguish between typing "pepper", and actually wanting a double tap
+        // action when hitting 'pp'. Suggested use case for this return value is when you want to send two
+        // keystrokes of the key, and not the 'double tap' action/macro.
+        if (state->interrupted) return TD_DOUBLE_SINGLE_TAP;
+        else if (state->pressed) return TD_DOUBLE_HOLD;
+        else return TD_DOUBLE_TAP;
+    } else return TD_UNKNOWN;
+}
+
+void delta_finished(tap_dance_state_t *state, void *user_data) {
+    delta_tap_state.state=cur_dance(state);
+    switch (delta_tap_state.state){
+        case TD_SINGLE_HOLD:
+	  register_mods(MOD_LSFT);
+            break;
+    case TD_DOUBLE_TAP:
+      if(isShiftLocked) {
+	isShiftLocked=false;
+	unregister_mods(MOD_LSFT);
+      } else {
+	isShiftLocked=true;
+	register_mods(MOD_LSFT);
+      }
+     break;
+    default:
+            break;
+    }
+}
+
+void delta_reset(tap_dance_state_t *state, void *user_data) {
+    switch (delta_tap_state.state ) {
+    case TD_SINGLE_HOLD:
+      unregister_mods(MOD_LSFT);
+	break;
+    default:
+      break;
+    }
+    
+    delta_tap_state.state = TD_NONE;
+}

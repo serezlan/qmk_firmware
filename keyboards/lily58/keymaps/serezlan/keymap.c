ad792cd80c1eb66f05771932efdd80fe4e979e7b
Edhoari Setiyoso <edhoari@raptor>
2025-11-08 14:01:01 +0700

    feat: rewo

--- keyboards/lily58/keymaps/serezlan/keymap.c
+++ keyboards/lily58/keymaps/serezlan/keymap.c
@@ -69,8 +69,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		    TD(TD_GRV_MEDIA_LAYER),   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQUAL,
   KC_INS,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  TD(TD_SHIFT_LOCK),  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_ENT,
-		    KC_LALT, KC_LGUI, KC_LCTL, MO(_LOWER), TD(TD_BSPC_RAISE_LAYER), KC_SPC, KC_RALT, KC_MENU
+  TD(TD_SHIFT_LOCK),  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_BSPC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_ENT,
+		    KC_LALT, KC_LGUI, KC_LCTL, MO(_LOWER), MO(_RAISE), KC_SPC, KC_RALT, KC_MENU
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -110,9 +110,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_RAISE] = LAYOUT(
   _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-  KC_F1,  KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,                       XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX,
-  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   _______, _______,  KC_PLUS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+  XXXXXXX,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    XXXXXXX,                        XXXXXXX,    KC_PGDN,    KC_UP,    KC_PGUP,    XXXXXXX,    _______,
+  XXXXXXX,  KC_F5,    KC_F6,   KC_F7,   KC_F8,   XXXXXXX,                       KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT,   KC_END, XXXXXXX,
+  XXXXXXX,   KC_F9,   KC_F10,   KC_F11,  KC_F12,  XXXXXXX,   _______, _______,  KC_PLUS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
                              _______, _______, _______,  _______, _______,  _______, _______, _______
 ),
 
